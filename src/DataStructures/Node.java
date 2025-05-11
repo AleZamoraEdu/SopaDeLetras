b0f106ff594317983a15e79db38b9a294893a709
AleZamoraEdu <alezamora247@gmail.com>
2025-05-11 14:48:32 -0400

    Add java interface IList and some descriptions for Node

--- src/DataStructures/Node.java
+++ src/DataStructures/Node.java
@@ -5,35 +5,65 @@
 package DataStructures;
 
 /**
+ * Represents a node in a singly linked list
  *
  * @author Alejandro Zamora
- * @param <T>
+ * @param <T> the type of data stored in the node
  */
 public class Node<T> {
 
     private T data;
     private Node<T> next;
 
+    /**
+     * Constructs a new node with the specified data.
+     * The next is initialized to null.
+     * @param data the data to be stored in this node
+     */
     public Node(T data) {
         this.data = data;
         this.next = null;
     }
 
-    // Getters
+    /**
+     * Returns the data stored in this node.
+     * @return the data stored in this node
+     */
     public T getData() {
         return data;
     }
-
-    public Node<T> getNext() {
-        return next;
-    }
-
-    // Setters
+    
+    /**
+     * Sets the data for this node.
+     * @param data the new data to be stored in this node
+     */
     public void setData(T data) {
         this.data = data;
     }
 
+    /**
+     *  Returns the next node in the list.
+     * @return the next node, or null if this is the last node
+     */
+    public Node<T> getNext() {
+        return next;
+    }
+    
+    /**
+     * Sets the next node in the list.
+     * @param next the node that should follow this node
+     */
     public void setNext(Node<T> next) {
         this.next = next;
+    }
+    
+    @Override
+    public String toString() {
+        return "Node{" +
+                "data=" + (data != null ? data.toString() : "null")
+                +
+                // Avoid recursively printing the whole list in toString of Node
+                ", next=" + (next != null ? "Node@" + 
+                Integer.toHexString(next.hashCode()) : "null");
     }
 }
